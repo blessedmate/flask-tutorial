13b20099ac0380de8aaea2997c5c936f31c44543
Mate <md.quintero@uniandes.edu.co>
2023-09-06 10:51:54 -0500

    flashing messages

---
+++ 6_flash_messages.py
@@ -0,0 +1,49 @@
+from flask import Flask, redirect, url_for, render_template, request, session, flash
+from datetime import timedelta
+
+app = Flask(__name__)
+app.secret_key = "AJK"
+app.permanent_session_lifetime = timedelta(days=5)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html", content="Testing")
+
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        session.permanent = True
+        user = request.form["nm"]
+        session["user"] = user
+        flash("Login succesful")
+        return redirect(url_for("user"))
+    else:
+        if "user" in session:
+            flash("Already logged in...")
+            return redirect(url_for("user"))
+        return render_template("login.html")
+
+
+@app.route("/user")
+def user():
+    if "user" in session:
+        user = session["user"]
+        return render_template("user.html", user=user)
+    else:
+        flash("You are not logged in")
+        return redirect(url_for("login"))
+
+
+@app.route("/logout")
+def logout():
+    if "user" in session:
+        user = session["user"]
+        flash(f"You have been successfuly logged out, {user}", "info")
+    session.pop("user", None)
+    return redirect(url_for("login"))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
